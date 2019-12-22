23e750883dbe525051d1056b0f4750bd472f5c44
alfille <paul.alfille@gmail.com>
2019-12-21 22:02:49 -0500

    Added different strategies The best is -s1 which uses subsquare constraints

--- suduku_count.c
+++ suduku_count.c
@@ -9,18 +9,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 // SIZE x SIZE suduku board
 #define SIZE (9)
 # define SUBSIZE (3) // sqrt of SIZE
+# define TOTALSIZE (SIZE*SIZE)
 
 #define Zero(array) memset( array, 0, sizeof(array) ) ;
 
 typedef enum { Good_Square, Candidate_Square, Bad_Square } square_ret ;
+clock_t start ;
 
 // bit pattern
 int pattern[SIZE] ;
 int full_pattern ;
+
+// Make numbers int bit pattern (1=0x1 2=0x2 3=0x4...)
 void make_pattern(void) {
     int i ;
     full_pattern = 0 ;
@@ -29,6 +35,8 @@ void make_pattern(void) {
         full_pattern |= pattern[i] ;
     }
 }
+
+// Find number from bit pattern (for printing)
 int reverse_pattern( int pat ) {
     int i ;
     for (i=0 ; i<SIZE;++i) {
@@ -58,26 +66,26 @@ int find_valid_bit( int mask ) {
 void print_square( void ) {
     int i ;
     int j ;
-    printf("\n");
+    fprintf(stderr,"\n");
     for ( j=0 ; j<SIZE ; ++j ) {
-        printf("+---");
+        fprintf(stderr,"+---");
     }
-    printf("+\n");
+    fprintf(stderr,"+\n");
     for (i=0 ; i<SIZE ; ++i ) {
         
         for ( j=0 ; j<SIZE ; ++j ) {
-            printf("|%2d ",reverse_pattern(bit[i][j]));
+            fprintf(stderr,"|%2d ",reverse_pattern(bit[i][j]));
         }
-        printf("|\n");
+        fprintf(stderr,"|\n");
         for ( j=0 ; j<SIZE ; ++j ) {
-            printf("+---");
+            fprintf(stderr,"+---");
         }
-        printf("+\n");
+        fprintf(stderr,"+\n");
     } 
-    printf("\n");
+    fprintf(stderr,"\n");
 }    
 
-square_ret fill_square( void ) {
+square_ret Type1_fill_square( void ) {
     int i,j ;
     int col_bits[SIZE] ;
     int row_bits[SIZE] ;
@@ -122,20 +130,13 @@ square_ret fill_square( void ) {
     return Good_Square ;
 }
 
-
-int main(void) {
+int Type1Loop() {
     int bad=0 ;
     int candidate=0 ;
     int good=0;
-    clock_t start ;
-    
-    SEED ;
-    make_pattern();
-    
-    start = clock() ;
 
     while ( 1 ) {
-        switch( fill_square() ) {
+        switch( Type1_fill_square() ) {
             case Bad_Square:
                 ++bad ;
                 break ;
@@ -156,6 +157,200 @@ int main(void) {
                 break ;
         }
     }
-    printf("Bad=%d, Candidate=%d, Good=%d\n\t\t%.2f%%\t%.2f%%\n",bad,candidate,good,(100.*candidate)/(bad+candidate+good),(100.*good)/(bad+candidate+good)) ;
+}
+
+square_ret Type2_fill_square( void ) {
+    int i,j,k ;
+    int col_bits[SIZE] ;
+    int row_bits[SIZE] ;
     
+    Zero(bit) ;
+    
+    // column bits culmulative
+    Zero( col_bits ) ;
+    
+    // row bits culmulative
+    Zero( row_bits ) ;
+    
+    // Fill columns and rows
+    for (k=0;k<SIZE;++k) {
+        for (j=k;j<SIZE;++j) {
+            int b = find_valid_bit( col_bits[j]|row_bits[k] ) ;
+            if (b == 0 ) {
+                return Bad_Square ;
+            }
+            row_bits[k] |= b ;
+            col_bits[j] |= b ;
+            bit[k][j] = b ;
+		}
+        for (i=k+1;i<SIZE;++i) {
+            int b = find_valid_bit( col_bits[k]|row_bits[i] ) ;
+            if (b == 0 ) {
+                return Bad_Square ;
+            }
+            row_bits[i] |= b ;
+            col_bits[k] |= b ;
+            bit[i][k] = b ;
+		}
+    }
+    
+    // Test subsquares
+    for( i=0 ; i<SIZE ; i+=SUBSIZE) {
+        for( j=0 ; j<SIZE ; j+=SUBSIZE ) {
+            int sub_bits = 0 ;
+            int si,sj;
+            for (si=0 ; si<SUBSIZE ; ++si ) {
+                for (sj=0 ; sj<SUBSIZE ; ++sj ) {
+                    sub_bits |= bit[i+si][j+sj] ;
+                }
+            }
+            if ( sub_bits != full_pattern ) {
+                return Candidate_Square ;
+            }
+        }
+    } 
+        return Good_Square ;
+}
+
+int Type2Loop() {
+    int bad=0 ;
+    int candidate=0 ;
+    int good=0;
+
+    while ( 1 ) {
+        switch( Type2_fill_square() ) {
+            case Bad_Square:
+                ++bad ;
+                break ;
+            case Candidate_Square:
+                ++candidate ;
+                if ( candidate % 10000 == 0 ) {
+					int total = bad+candidate+good ;
+                    printf("Bad=%d, Candidate=%d, Good=%d\tper second=%g.2\n\t\t%.6f%%\t%.6f%%\n",bad,candidate,good,(double)(CLOCKS_PER_SEC*total)/(clock()-start),(100.*candidate)/total,(100.*good)/total) ;
+                }
+                break ;
+            case Good_Square:
+                ++good ;
+                print_square() ;
+                if ( 1 ) {
+					int total = bad+candidate+good ;
+                    printf("Bad=%d, Candidate=%d, Good=%d\tper second=%g.2\n\t\t%.6f%%\t%.6f%%\n",bad,candidate,good,(double)(CLOCKS_PER_SEC*total)/(clock()-start),(100.*candidate)/total,(100.*good)/total) ;
+                }
+                break ;
+        }
+    }
+}
+
+int SS1_fill_square( void ) {
+    int si,sj,ssi,ssj,count=0 ;
+    int col_bits[SIZE] ;
+    int row_bits[SIZE] ;
+    int ss_bits[SUBSIZE][SUBSIZE] ;
+    
+    Zero(bit) ;
+    
+    // column bits culmulative
+    Zero( col_bits ) ;
+    
+    // row bits culmulative
+    Zero( row_bits ) ;
+    
+    // subsquare bits culmulative
+    Zero( ss_bits ) ;
+    
+    // Fill columns and rows
+    for (si=0;si<SUBSIZE;++si) {
+        for (sj=0;sj<SUBSIZE;++sj) {
+			for (ssi=0;ssi<SUBSIZE;++ssi) {
+				for ( ssj=0;ssj<SUBSIZE;++ssj,++count) {
+					int i = SUBSIZE*si+ssi ;
+					int j = SUBSIZE*sj+ssj ;
+					int b = find_valid_bit( col_bits[j]|row_bits[i]|ss_bits[si][sj] ) ;
+					if (b == 0 ) {
+						return count ;
+					}
+					row_bits[i] |= b ;
+					col_bits[j] |= b ;
+					ss_bits[si][sj] |= b ;
+					bit[i][j] = b ;
+				}
+			}
+		}
+    }
+	return count ;
+}
+
+int SS1Loop() {
+	int count = 0 ;
+	int good = 0 ;
+	uint64_t totalcount = 0 ;
+
+    while ( 1 ) {
+		int c = SS1_fill_square() ;
+		++count ;
+		totalcount += c ;
+		if ( c == TOTALSIZE ) {
+			++good ;
+			print_square() ;
+			printf("count=%d, Good=%d\taverage=%g.1\tper second=%.1f\n\t\t%.6f%%\n",count,good,(double)totalcount/count,(double)(CLOCKS_PER_SEC*count)/(clock()-start),(100.*good)/count) ;
+		} else if ( count % 100000 == 0 ) {
+			printf("count=%d, Good=%d\taverage=%g.1\tper second=%.1f\n\t\t%.6f%%\n",count,good,(double)totalcount/count,(double)(CLOCKS_PER_SEC*count)/(clock()-start),(100.*good)/count) ;
+		}
+    }
+}
+
+void help(void) {
+	printf(
+	"suduku_count by Paul H Alfille 2019 MIT License\n"
+	"\tSearch the possible suduku space for valid positions\n"
+	"\tUses several search algorithms and shows statistics\n"
+	"\tSee https:\\github.com/alfille/suduku_count\n"
+	"\n"
+	"\t -t 1\tSearch columns first, then test for subsquares (default)\n"
+	"\t -t 2\tSearch columns/row alternating, then test for subsquares\n"
+	"\t -s 1\tSearch subsquares by column, show failure point\n"
+	"\t -h  \tShow these instructions\n"
+	);
+}
+
+int main(int argc, char ** argv) {
+	int c ;
+
+    SEED ;
+    make_pattern();
+        
+    start = clock() ;
+
+	do {
+		switch( getopt( argc, argv, "ht:s:" ) ) {
+			case 't':
+				switch (atoi(optarg)) {
+					case 2:
+						Type2Loop();
+						break ;
+					default:
+						Type1Loop() ;
+						break ;
+				}
+				break ;
+			case 's':
+				switch (atoi(optarg)) {
+					case 1:
+						SS1Loop();
+						break ;
+					default:
+						SS1Loop() ;
+						break ;
+				}
+				break ;
+			case -1:
+				Type1Loop();
+				break;
+			case 'h':
+			default:
+				help() ;
+				break;
+		} 
+	} while (0) ;
+	return 0 ;
 }
